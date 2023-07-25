26860029f4bdaec669a8d19ec9d95828c5bbd2e6
sura <45725129+surashirishi@users.noreply.github.com>
2023-07-25 20:10:03 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,20 +1,22 @@
 import bottle
-from bottle import route, run, Response, template
+from bottle import Bottle, route, run, Response, template
 import json
 import image
+
+app = Bottle()
 
 def call_service():
     directoryName = 'photos'
     image.process(directoryName)
 
 @route('/')
-def app():
+def index():
     """Home page"""
     title = "Image Processor App"
     call_service()
     return template('index.tpl',data="Request completed!", title=title)
 
 if __name__ == '__main__':
-	run(host='0.0.0.0', port=8000, debug=False, reloader=True)
+	run(app, host='0.0.0.0', port=8000, debug=False, reloader=True)
 	
 serverApp = bottle.default_app()
