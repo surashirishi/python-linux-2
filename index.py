f24edb12f6f555ed15fa026766b0eb1807bfeb02
sura <45725129+surashirishi@users.noreply.github.com>
2023-07-27 13:45:05 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,14 @@
 import bottle
 from bottle import Bottle, route, run, Response, template
 import json
-import image
+
+import cProfile, pstats, io
+from pstats import SortKey
 
 app = Bottle()
+
+pr = cProfile.Profile()
+pr.enable()
 
 def call_service():
     directoryName = 'photos'
@@ -20,3 +25,10 @@ if __name__ == '__main__':
 	run(app, host='0.0.0.0', port=8000, debug=False, reloader=True)
 	
 serverApp = bottle.default_app()
+
+pr.disable()
+s = io.StringIO()
+sortby = SortKey.CUMULATIVE
+ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps.print_stats()
+print(s.getvalue())
