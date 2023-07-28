1dd2cbda0233a7f0cecc1056cca6c7a138fb5f77
sura <45725129+surashirishi@users.noreply.github.com>
2023-07-28 13:48:50 +0900

    Update index.py

--- index.py
+++ index.py
@@ -6,21 +6,14 @@ import image
 import cProfile, pstats, io
 from pstats import SortKey
 
+pr = cProfile.Profile()
+pr.enable()
+
 app = Bottle()
 
 def call_service():
-    pr = cProfile.Profile()
-    pr.enable()
-
     directoryName = 'photos'
     image.process(directoryName)
-    
-    pr.disable()
-    s = io.StringIO()
-    sortby = SortKey.CUMULATIVE
-    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-    ps.print_stats()
-    print(s.getvalue())
 
 @app.route('/')
 def index():
@@ -33,3 +26,10 @@ if __name__ == '__main__':
 	run(app, host='0.0.0.0', port=8000, debug=False, reloader=True)
 	
 serverApp = bottle.default_app()
+
+pr.disable()
+s = io.StringIO()
+sortby = SortKey.CUMULATIVE
+ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+ps.print_stats()
+print(s.getvalue())
