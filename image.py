224eab41efa1f0410da452caa8226ce4995f0a32
sura <45725129+surashirishi@users.noreply.github.com>
2023-07-28 12:43:57 +0900

    Update image.py

--- image.py
+++ image.py
@@ -5,13 +5,7 @@ import matplotlib.pyplot as plt
 from skimage.color import rgb2gray
 import helper
 
-import cProfile, pstats, io
-from pstats import SortKey
-
 def process(directoryName):
-    pr = cProfile.Profile()
-    pr.enable()
-
     for filename in os.listdir(directoryName):
         if filename.endswith(".jpg") or filename.endswith(".png"):
             filenamestr = filename.split('.')[0]
@@ -25,10 +19,3 @@ def process(directoryName):
             imsave(os.getcwd() + "/output/" + filenamestr +"-" + helper.generateRandomName() + "_modified.jpeg", (img_new*255).astype(np.uint8))
         else:
             continue
-        
-        pr.disable()
-        s = io.StringIO()
-        sortby = SortKey.CUMULATIVE
-        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-        ps.print_stats()
-        print(s.getvalue())
